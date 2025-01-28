397caeb832ad39d1b524c469487d6698addded04
SilasR <sidunc19@gmail.com>
2025-01-28 17:40:09 -0500

    Reorganizing dahsboard.java layout

--- src/main/java/frc/robot/Dashboard.java
+++ src/main/java/frc/robot/Dashboard.java
@@ -19,40 +19,39 @@ public class Dashboard {
     @SuppressWarnings("unused")
     private NetworkTable table;
 
-    // Array publishers
-    public static StringArrayPublisher legalActuatorNames;
-    public static StringArrayPublisher legalDrivers;
+    // Drivebase Subsystem
     public static DoubleArrayPublisher swerve0Details;
     public static DoubleArrayPublisher swerve1Details;
     public static DoubleArrayPublisher swerve2Details;
     public static DoubleArrayPublisher swerve3Details;
     public static DoubleArrayPublisher currentDriverProfileSetpoints;
+    public static StringArrayPublisher legalDrivers;
+
+    public static DoubleArraySubscriber newDriverProfileSetpoints;
+    
+    public static DoubleSubscriber selectedDriver;
+    public static BooleanSubscriber unlockAzimuth;
+    public static BooleanSubscriber homeWheels;
+    public static BooleanSubscriber calibrateWheels;
+    public static BooleanSubscriber applyProfileSetpoints;
+
+    // Other Testing
+    public static StringArrayPublisher legalActuatorNames;
     public static BooleanArrayPublisher confirmedMasterStates;
 
-    // Publishers
+    public static DoublePublisher pressureTransducer;
+    public static DoublePublisher loopTime;
     public static StringPublisher robotProfile;
     public static StringPublisher codeVersion;
-    public static DoublePublisher pressureTransducer;
-
     public static BooleanPublisher isAutonomous;
-    public static DoublePublisher loopTime;
-
-    // Array Subscribers
-    public static DoubleArraySubscriber newDriverProfileSetpoints;
-
-    // Subscribers
-    public static StringSubscriber testActuatorName;
+    
     public static DoubleSubscriber testActuatorValue;
     public static DoubleSubscriber testActuatorPeriod;
-    public static DoubleSubscriber selectedDriver;
     public static DoubleSubscriber freeTuningVariable;
     public static DoubleSubscriber freeTuningkP;
     public static DoubleSubscriber freeTuningkI;
     public static DoubleSubscriber freeTuningkD;
-    public static BooleanSubscriber unlockAzimuth;
-    public static BooleanSubscriber applyProfileSetpoints;
-    public static BooleanSubscriber homeWheels;
-    public static BooleanSubscriber calibrateWheels;
+    public static StringSubscriber testActuatorName;
 
     /**
      * Creates an object for storing dashboard publishers and subscribers
@@ -67,9 +66,9 @@ public class Dashboard {
         swerve2Details = table.getDoubleArrayTopic("Swerve_2_Details").publish();
         swerve3Details = table.getDoubleArrayTopic("Swerve_3_Details").publish();
         currentDriverProfileSetpoints = table.getDoubleArrayTopic("Current_Driver_Profile_Setpoints").publish();
-        legalDrivers = table.getStringArrayTopic("Legal_Drivers").publish();
         newDriverProfileSetpoints = table.getDoubleArrayTopic("New_Driver_Profile_Setpoints")
                 .subscribe(new double[] { 0.08, 1.8, 1, 0.15, 2.5, 1 });
+        legalDrivers = table.getStringArrayTopic("Legal_Drivers").publish();
 
         selectedDriver = table.getDoubleTopic("Selected_Driver").subscribe(0.0);
         unlockAzimuth = table.getBooleanTopic("Unlock_Azimuth").subscribe(false);
@@ -82,17 +81,18 @@ public class Dashboard {
         legalActuatorNames = table.getStringArrayTopic("Legal_Actuator_Names").publish();
         confirmedMasterStates = table.getBooleanArrayTopic("Confirmed_States").publish();
 
-        robotProfile = table.getStringTopic("Robot_Profile").publish();
-        codeVersion = table.getStringTopic("Robot_Code_Version").publish();
         pressureTransducer = table.getDoubleTopic("Pressure_Transducer").publish();
         loopTime = table.getDoubleTopic("Control_Loop_Time").publish();
+        robotProfile = table.getStringTopic("Robot_Profile").publish();
+        codeVersion = table.getStringTopic("Robot_Code_Version").publish();
         isAutonomous = table.getBooleanTopic("Robot_is_Autonomous").publish();
-        testActuatorName = table.getStringTopic("Test_Actuator_Name").subscribe("");
+
         testActuatorValue = table.getDoubleTopic("Test_Actuator_Value").subscribe(0.0);
         testActuatorPeriod = table.getDoubleTopic("Test_Actuator_Period").subscribe(0.0);
         freeTuningVariable = table.getDoubleTopic("Free_Tuning_Variable").subscribe(0);
         freeTuningkP = table.getDoubleTopic("Free_Tuning_PID_P").subscribe(0);
         freeTuningkI = table.getDoubleTopic("Free_Tuning_PID_I").subscribe(0);
         freeTuningkD = table.getDoubleTopic("Free_Tuning_PID_D").subscribe(0);
+        testActuatorName = table.getStringTopic("Test_Actuator_Name").subscribe("");
     }
 }
